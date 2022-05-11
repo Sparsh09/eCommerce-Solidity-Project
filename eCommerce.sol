33c89451736a4da8f9c07ee84a387b9c3d8c566f
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2022-05-11 12:50:11 +0530

    added destroy method

--- eCommerce.sol
+++ eCommerce.sol
@@ -15,13 +15,25 @@ contract eCommerceProject {
 
     uint counter = 1;
     Product[] public products;
+    address payable public manager;
+    bool isDestroyed = false;
+
+    modifier isNotDestroyed(){
+        require(!isDestroyed , "Contract does not exist");
+        _;
+    }
+
+
+    constructor () {
+        manager = payable(msg.sender);
+    }
 
     event registerd(string title , uint productId , address seller);
     event bought(uint productId, address buyer);
     event delivered(uint productId);
     event CheckPrice(uint senValue , uint orignalValue);
 
-    function register(string memory _title, string memory _desc , uint _price) public {
+    function register(string memory _title, string memory _desc , uint _price) public isNotDestroyed {
         require(_price > 0 , "Should be greater than zero");
         Product memory tempProduct ;
         tempProduct.title = _title;
@@ -33,18 +45,33 @@ contract eCommerceProject {
         emit registerd(_title , tempProduct.productId , msg.sender);
     }
 
-    function buy(uint _productId) payable public {
+    function buy(uint _productId) payable public isNotDestroyed{
         require(products[_productId-1].price == msg.value, "Please pay the exact price");
         require(products[_productId-1].seller != msg.sender , "Seller cannot buy its own product");
         products[_productId-1].buyer = msg.sender;
         emit bought(_productId, msg.sender);
     }
     
-    function delivery(uint _productId) public {
+    function delivery(uint _productId) public isNotDestroyed{
         require(products[_productId-1].buyer == msg.sender , "only buyer can confirm");
         products[_productId - 1].isDelivered = true;
         products[_productId - 1].seller.transfer(products[_productId - 1].price);
         emit delivered(_productId);
+    }
+
+    // function destroy() public {
+    //     require(msg.sender == manager , "Only manager can call this method");
+    //     selfdestruct(manager);
+    // }
+
+    function destroy() public isNotDestroyed{
+        require(msg.sender == manager , "Only manager can call this method");
+        manager.transfer(address(this).balance);
+        isDestroyed = true;
+    }
+
+    fallback() payable external{
+        payable(msg.sender).transfer(msg.value);
     }
 
 }
